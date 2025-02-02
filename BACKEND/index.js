58183d04ba3c8b26c1fd4d8ee061f0a1eab21733
sadat <shameemsadat00@gmail.com>
2025-02-02 21:01:47 +0600

    added cors in backend

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,4 +1,5 @@
 import express, { json } from "express";
+import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import todoRouter from "./routes/todosRoutes.js";
@@ -6,10 +7,17 @@ import todoRouter from "./routes/todosRoutes.js";
 
 
 const app = express();
+
 connectDB();
 
-
 app.use(express.json());
+
+
+const clientURL= `${process.env.BASE_URL}:${process.env.CLIENT_PORT}`;
+app.use(cors({
+    origin: clientURL,
+    credentials: true, // Allow cookies and authentication headers if needed
+})); 
 
 
 app.get('/',(req,res)=>{
@@ -21,8 +29,8 @@ app.use('/api',todoRouter);
 
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.SERVER_PORT || 8000;
 app.listen(port,()=>{
-    console.log(`server running on ${process.env.BACKEND_URL}:${port}`);
+    console.log(`server running on ${process.env.BASE_URL}:${port}`);
     
 })
